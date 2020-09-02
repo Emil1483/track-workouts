b352f5554ac875f472dcec09bd9475c8e8511914
Emil Djupvik <emil14833@gmail.com>
2020-09-02 22:39:51 +0200

    fix hot reload for html

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,24 +1,42 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './src/client.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'lib'),
-        publicPath: 'lib'
-    },
-    devtool: 'inline-source-map',
     module: {
         rules: [
             {
                 test: /\.ts$/,
+                include: [path.resolve(__dirname, 'src')],
                 use: 'ts-loader',
-                exclude: /node_modules/
             }
         ]
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
+    },
+    devtool: 'eval-source-map',
+    output: {
+        publicPath: 'lib',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'lib'),
+    },
+    devServer: {
+        hot: true,
+        contentBase: path.join(__dirname, '/'),
+        watchContentBase: true,
+        before(app, server, compiler) {
+            const watchFiles = ['.html', '.hbs'];
+
+            compiler.plugin('done', () => {
+                const changedFiles = Object.keys(compiler.watchFileSystem.watcher.mtimes);
+
+                if (
+                    this.hot &&
+                    changedFiles.some(filePath => watchFiles.includes(path.parse(filePath).ext))
+                ) {
+                    server.sockWrite(server.sockets, 'content-changed');
+                }
+            });
+        }
     }
 };
